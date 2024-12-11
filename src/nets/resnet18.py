a31b16cd0f4d7662a763c37db2a36540258f7253
rdk31 <rdk31@protonmail.com>
2024-12-11 21:03:40 +0100

    resnet pretrained

--- src/nets/resnet18.py
+++ src/nets/resnet18.py
@@ -1,12 +1,19 @@
 import torch.nn as nn
-from torchvision.models import resnet18
+from torchvision.models import ResNet18_Weights, resnet18
 
 
 class Resnet18(nn.Module):
-    def __init__(self, num_classes: int):
+    def __init__(self, num_classes: int, pretrained: bool):
         super().__init__()
 
-        self.base_model = resnet18(weights=None, num_classes=num_classes)
+        if pretrained:
+            self.base_model = resnet18(weights=ResNet18_Weights.DEFAULT)
+            if num_classes != 1000:
+                self.base_model.fc = nn.Linear(
+                    self.base_model.fc.in_features, num_classes
+                )
+        else:
+            self.base_model = resnet18(num_classes=num_classes)
 
     def forward(self, x):
         return self.base_model(x)
